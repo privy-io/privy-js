84be686b64a4d96d80fbc824198472e32d7f4226
Zachary Liu <zachary@zacharyliu.com>
2022-05-21 03:52:30 -0400

    Remove session storage from Node SDK

--- packages/privy-node/src/sessions/custom.ts
+++ packages/privy-node/src/sessions/custom.ts
@@ -2,17 +2,12 @@ import {Session} from '.';
 import {Token} from '../token';
 import {RunEffectOnce} from '../effect';
 import {PrivySessionError} from '../errors';
-import storage from '../storage';
 
 // By default, a session will be considered unauthenticated
 // 30 seconds prior to its token's expiration time. This is
 // so we can eagerly re-authenticate before the server would
 // reject requests with a 401.
 const DEFAULT_EXPIRATION_PADDING_IN_SECONDS = 30;
-
-// Store the privy token under this key so that it
-// can persist between page refreshes and browser tabs.
-const TOKEN_STORAGE_KEY = 'privy:token';
 
 /**
  * `CustomSession` implements the {@link Session} interface. `CustomSession` can be used to authenticate to Privy through your own backend.
@@ -33,18 +28,11 @@ export class CustomSession implements Session {
    * @param authenticate Custom authenticate function. Must return a valid JWT on success.
    */
   constructor(authenticate: () => Promise<string>) {
-    // Attempt to hydrate the session by reading the
-    // session token from storage if it exists.
-    const token = storage.get(TOKEN_STORAGE_KEY);
-    this.token = typeof token === 'string' ? token : null;
-
     this.expirationPaddingInSeconds = DEFAULT_EXPIRATION_PADDING_IN_SECONDS;
 
     this.authenticateOnce = new RunEffectOnce(async () => {
       try {
-        const token = await authenticate();
-        this.token = token;
-        storage.put(TOKEN_STORAGE_KEY, token);
+        this.token = await authenticate();
       } catch (error) {
         throw new PrivySessionError(`Error authenticating session: ${error}`);
       }
@@ -53,7 +41,6 @@ export class CustomSession implements Session {
     this.destroyOnce = new RunEffectOnce(async () => {
       try {
         this.token = null;
-        storage.del(TOKEN_STORAGE_KEY);
       } catch (error) {
         throw new PrivySessionError(`Error destroying session: ${error}`);
       }
