643c86fba0193ec670ca7ff5d0f05db2c24a291f
dandydavey <90277583+dandydavey@users.noreply.github.com>
2022-05-26 17:39:19 -0400

    Revert pagination changes in node to unblock shipping (#46)

--- packages/privy-node/src/index.ts
+++ packages/privy-node/src/index.ts
@@ -3,10 +3,8 @@ import {PrivyClient} from './client';
 export {Session} from './sessions';
 export {CustomSession} from './sessions/custom';
 
-export {FieldInstance, BatchFieldInstances, UserFieldInstances} from './fieldInstance';
+export {FieldInstance} from './fieldInstance';
 
 export {PrivyError, PrivyApiError, PrivyClientError, PrivySessionError} from './errors';
-
-export {BatchOptions} from './types';
 
 export default PrivyClient;
