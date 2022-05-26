3b246e2c272b6d876884337cb79144708769c246
Asta <74164885+asta-li@users.noreply.github.com>
2022-05-26 17:40:51 -0400

    Updates

--- packages/privy-node/src/index.ts
+++ packages/privy-node/src/index.ts
@@ -7,5 +7,3 @@ export {CustomSession} from './sessions/custom';
 export {FieldInstance} from './fieldInstance';
 
 export {PrivyError, PrivyApiError, PrivyClientError, PrivySessionError} from './errors';
-
-export default PrivyClient;
